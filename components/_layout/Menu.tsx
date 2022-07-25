0356b265394e405134deab4a9d936a1f504f4c9f
bluepnwage <a.carty2555@gmail.com>
2022-07-25 16:41:00 -0300

    updated mantine to v5

--- components/_layout/Menu.tsx
+++ components/_layout/Menu.tsx
@@ -1,25 +1,14 @@
 import { Menu as MantineMenu, Avatar, useMantineColorScheme } from "@mantine/core";
 import { useStyles } from "./styles";
 import { User, Logout, Sun, Moon } from "tabler-icons-react";
-import { useState } from "react";
 import { checkTheme } from "util/theme";
+import { useDisclosure, useClickOutside } from "@mantine/hooks";
 
 export default function Menu() {
   const { classes, theme } = useStyles();
-  const [opened, setOpened] = useState(false);
   const { toggleColorScheme } = useMantineColorScheme();
-
-  const handler = {
-    off() {
-      setOpened(false);
-    },
-    on() {
-      setOpened(true);
-    },
-    toggle() {
-      setOpened((prev) => !prev);
-    }
-  };
+  const [opened, handler] = useDisclosure(false);
+  const ref = useClickOutside(handler.close);
 
   const icon = checkTheme(
     theme,
@@ -29,30 +18,29 @@ export default function Menu() {
 
   return (
     <>
-      <MantineMenu
-        opened={opened}
-        onOpen={handler.on}
-        onClose={handler.off}
-        closeOnItemClick={false}
-        control={
+      <MantineMenu opened={opened} onOpen={handler.open} onClose={handler.close}>
+        <MantineMenu.Target>
           <Avatar
             className={classes.menuControl}
             src={"/bluepnwage.jpg"}
             radius={"xl"}
             imageProps={{ loading: "lazy" }}
           />
-        }
-      >
-        <MantineMenu.Label>Profile</MantineMenu.Label>
-        <MantineMenu.Item icon={<User size={16} />}>Dashboard</MantineMenu.Item>
-        <MantineMenu.Item onClick={() => toggleColorScheme()} icon={icon}>
-          Change theme
-        </MantineMenu.Item>
-        <MantineMenu.Item
-          icon={<Logout color={checkTheme(theme, theme.colors.red[7], theme.colors.red[4])} size={16} />}
-        >
-          Sign out
-        </MantineMenu.Item>
+        </MantineMenu.Target>
+        <MantineMenu.Dropdown>
+          <div ref={ref}>
+            <MantineMenu.Label>Profile</MantineMenu.Label>
+            <MantineMenu.Item icon={<User size={16} />}>Dashboard</MantineMenu.Item>
+            <MantineMenu.Item closeMenuOnClick={false} onClick={() => toggleColorScheme()} icon={icon}>
+              Change theme
+            </MantineMenu.Item>
+            <MantineMenu.Item
+              icon={<Logout color={checkTheme(theme, theme.colors.red[7], theme.colors.red[4])} size={16} />}
+            >
+              Sign out
+            </MantineMenu.Item>
+          </div>
+        </MantineMenu.Dropdown>
       </MantineMenu>
     </>
   );
