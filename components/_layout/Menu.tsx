a9b4261e12271c9262d9abaa2f35483e6e037cab
bluepnwage <a.carty2555@gmail.com>
2022-08-01 16:49:03 -0300

    updated mantine to latest patch

--- components/_layout/Menu.tsx
+++ components/_layout/Menu.tsx
@@ -2,7 +2,7 @@ import { Menu as MantineMenu, Avatar, useMantineColorScheme } from "@mantine/cor
 import { useStyles } from "./styles";
 import { User, Logout, Sun, Moon } from "tabler-icons-react";
 import { checkTheme } from "util/theme";
-import { useDisclosure, useClickOutside } from "@mantine/hooks";
+import { useDisclosure } from "@mantine/hooks";
 import { NextLink } from "@mantine/next";
 import { memo } from "react";
 
@@ -10,7 +10,6 @@ function Menu() {
   const { classes, theme } = useStyles();
   const { toggleColorScheme } = useMantineColorScheme();
   const [opened, handler] = useDisclosure(false);
-  const ref = useClickOutside(handler.close);
 
   const icon = checkTheme(
     theme,
@@ -35,21 +34,19 @@ function Menu() {
           />
         </MantineMenu.Target>
         <MantineMenu.Dropdown>
-          <div ref={ref}>
-            <MantineMenu.Label>Profile</MantineMenu.Label>
-            <MantineMenu.Item component={NextLink} href={"/dashboard/bluepnwage"} icon={<User size={16} />}>
-              Dashboard
-            </MantineMenu.Item>
-            <MantineMenu.Item closeMenuOnClick={false} onClick={() => toggleColorScheme()} icon={icon}>
-              Change theme
-            </MantineMenu.Item>
-            <MantineMenu.Item
-              onClick={() => logOut()}
-              icon={<Logout color={checkTheme(theme, theme.colors.red[7], theme.colors.red[4])} size={16} />}
-            >
-              Sign out
-            </MantineMenu.Item>
-          </div>
+          <MantineMenu.Label>Profile</MantineMenu.Label>
+          <MantineMenu.Item component={NextLink} href={"/dashboard/bluepnwage"} icon={<User size={16} />}>
+            Dashboard
+          </MantineMenu.Item>
+          <MantineMenu.Item closeMenuOnClick={false} onClick={() => toggleColorScheme()} icon={icon}>
+            Change theme
+          </MantineMenu.Item>
+          <MantineMenu.Item
+            onClick={() => logOut()}
+            icon={<Logout color={checkTheme(theme, theme.colors.red[7], theme.colors.red[4])} size={16} />}
+          >
+            Sign out
+          </MantineMenu.Item>
         </MantineMenu.Dropdown>
       </MantineMenu>
     </>
