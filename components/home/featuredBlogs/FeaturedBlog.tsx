ac3c32f442672b6b5b94227f533d0f40e7920e1b
bluepnwage <a.carty2555@gmail.com>
2022-08-05 22:14:34 -0300

    removed all suspense boundaries for featured blogs

--- components/home/featuredBlogs/FeaturedBlog.tsx
+++ components/home/featuredBlogs/FeaturedBlog.tsx
@@ -1,12 +1,10 @@
 import Link from "next/link";
 import { Anchor, Avatar, Group, Image, Stack, Text, Title, Skeleton } from "@mantine/core";
 import { useStyles } from "./styles";
-import { Suspense, lazy } from "react";
 import { Blog, User } from "@interfaces/supabase";
 import { formatDate } from "@util/formatDate";
 import { useUser } from "@hooks/useUser";
-
-const ProfileModal = lazy(() => import("@components/modal/ProfileModal"));
+import ProfileModal from "@components/modal/ProfileModal";
 
 interface PropTypes {
   blog: Blog;
@@ -20,57 +18,53 @@ export function FeaturedBlog({ blog, user: userData, onClose }: PropTypes) {
 
   const date = new Date(blog.created_at);
   return (
-    <Suspense fallback={null}>
-      <div className={cx("container", classes.blogContainer)}>
-        <figure className={classes.imageContainer}>
-          <Image
-            src={blog.thumbnail}
-            height={"100%"}
-            width={"100%"}
-            imageProps={{ loading: "lazy" }}
-            alt={"Philipsburg"}
-          />
-        </figure>
-        <div className={classes.descriptionContainer}>
-          <Text weight={400} className={classes.dimmedText} component={"time"}>
-            {formatDate(date)}
+    <div className={cx("container", classes.blogContainer)}>
+      <figure className={classes.imageContainer}>
+        <Image
+          src={blog.thumbnail}
+          height={"100%"}
+          width={"100%"}
+          imageProps={{ loading: "lazy" }}
+          alt={"Philipsburg"}
+        />
+      </figure>
+      <div className={classes.descriptionContainer}>
+        <Text weight={400} className={classes.dimmedText} component={"time"}>
+          {formatDate(date)}
+        </Text>
+        <Title mb={"md"} order={3}>
+          {blog.heading}
+        </Title>
+        <article>
+          <Text mb={"md"} component="p">
+            {blog.description}
           </Text>
-          <Title mb={"md"} order={3}>
-            {blog.heading}
-          </Title>
-          <article>
-            <Text mb={"md"} component="p">
-              {blog.description}
-            </Text>
-          </article>
-          <Link passHref href={`/blogs/${blog.id}`}>
-            <Anchor>Read article</Anchor>
-          </Link>
-          <Suspense fallback={null}>
-            <ProfileModal user={user} onClose={onClose}>
-              <Group mt={"md"}>
-                <Skeleton radius={"xl"} width={"fit-content"} visible={userLoading}>
-                  <Avatar
-                    imageProps={{ loading: "lazy" }}
-                    src={user.avatar_url}
-                    radius={"xl"}
-                    size={"lg"}
-                    alt={"Profile picture for author"}
-                  />
-                </Skeleton>
-                <Stack spacing={5}>
-                  <Text size="sm" component="strong">
-                    {user.first_name} {user.last_name}
-                  </Text>
-                  <Text size="sm" component="span" className={classes.dimmedText}>
-                    {user.occupation}
-                  </Text>
-                </Stack>
-              </Group>
-            </ProfileModal>
-          </Suspense>
-        </div>
+        </article>
+        <Link passHref href={`/blogs/${blog.id}`}>
+          <Anchor>Read article</Anchor>
+        </Link>
+        <ProfileModal user={user} onClose={onClose}>
+          <Group mt={"md"}>
+            <Skeleton radius={"xl"} width={"fit-content"} visible={userLoading}>
+              <Avatar
+                imageProps={{ loading: "lazy" }}
+                src={user.avatar_url}
+                radius={"xl"}
+                size={"lg"}
+                alt={"Profile picture for author"}
+              />
+            </Skeleton>
+            <Stack spacing={5}>
+              <Text size="sm" component="strong">
+                {user.first_name} {user.last_name}
+              </Text>
+              <Text size="sm" component="span" className={classes.dimmedText}>
+                {user.occupation}
+              </Text>
+            </Stack>
+          </Group>
+        </ProfileModal>
       </div>
-    </Suspense>
+    </div>
   );
 }
